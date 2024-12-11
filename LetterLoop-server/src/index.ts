d0b493da870a1a204a9ed58fde3361a2d8403634
ghNazim <160389831+ghNazim@users.noreply.github.com>
2024-12-11 21:11:19 +0530

    first commit basic setup

---
+++ LetterLoop-server/src/index.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, world!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
