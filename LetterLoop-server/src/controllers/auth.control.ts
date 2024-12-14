20a70b212b6349d909e4d0fc20abff8f93c589c3
ghNazim <160389831+ghNazim@users.noreply.github.com>
2024-12-14 23:22:12 +0530

    minor fix

--- LetterLoop-server/src/controllers/auth.control.ts
+++ LetterLoop-server/src/controllers/auth.control.ts
@@ -3,13 +3,14 @@ import { OAuth2Client } from "google-auth-library"
 import dotenv from "dotenv";
 dotenv.config();
 const redirectUrl = "http://localhost:3300/oauth/google/callback";
-const oClient = new OAuth2Client(
-  process.env.GOOGLE_CLIENT_ID,
-  process.env.GOOGLE_CLIENT_SECRET,
-  redirectUrl
-);
+
 
 export const handleAuthUrl = (req:Request, res:Response, next:NextFunction) => {
+  const oClient = new OAuth2Client(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    redirectUrl
+  );
 
     res.header('refferrer-policy', 'no-referrer-when-downgrade')
     
@@ -19,21 +20,27 @@ export const handleAuthUrl = (req:Request, res:Response, next:NextFunction) => {
       scope: "https://www.googleapis.com/auth/userinfo.profile",
       prompt: "consent",
     });
-    res.json({url:authorizeUrl});
-
+    res.status(200).json({url:authorizeUrl});
     next();
 }
 
-const getUserData = async (access_token:string)=>{
+const getUserData =async (access_token:string)=>{
   const response = fetch('https://www.googleapis.com/oauth2/v3/userinfo?access_token='+access_token)
   const data = (await response).json()
   console.log("user data: ",data)
 }
+
 export const handleGoogleCallback = async (req:Request,res:Response)=>{
   const code = req.query.code
   if(!code){
-    return res.status(404).json({message:"No code provided"})
+    res.status(404).json({message:"No code provided"})
+    return
   }
+  const oClient = new OAuth2Client(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    redirectUrl
+  );
   try {
     const response = await oClient.getToken(code as string);
     await oClient.setCredentials(response.tokens);
@@ -41,7 +48,8 @@ export const handleGoogleCallback = async (req:Request,res:Response)=>{
     const user = oClient.credentials;
     console.log("credentials:", user);
     await getUserData(user.access_token as string);
-    return res.status(200).json({message:"Success"})
+    res.status(200).json({message:"Success"})
+    return 
     
   } catch (error) {
     res.status(400).json({message:"Server Error",error:error})
